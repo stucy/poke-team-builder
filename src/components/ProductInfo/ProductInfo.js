e87fba0734886a31ead7a763aa8e20456d816e30
Stoyan Georgiev <stoyangeorgiev1999@gmail.com>
2020-10-08 16:42:59 +0300

    optimized code

--- src/components/ProductInfo/ProductInfo.js
+++ src/components/ProductInfo/ProductInfo.js
@@ -4,32 +4,15 @@ import { Link } from 'react-router-dom';
 import './ProductInfo.css';
 
 const ProductInfo = ({img, title, text, orientation, to}) => {
-    
-    let content = orientation == "left" ? (
-        <>
-            <img src={img} />
-            <div className="ProductInfoText">
-                <h1 className="ProductTitle">{title}</h1>
-                <p className="ProductText">{text}</p>
-                <Link to={to} className="ProductButton">View</Link>
-            </div>
-        </>
-    ) :
-
-    (
-        <>
-            <div className="ProductInfoText">
-                <h1 className="ProductTitle">{title}</h1>
-                <p className="ProductText">{text}</p>
-                <Link to={to} className="ProductButton">View</Link>
-            </div>
-            <img src={img} />
-        </>
-    );
 
     return (
-        <div className="ProductInfo">
-            {content}
+        <div className={`ProductInfo ${orientation}`}>
+            <img src={img} />
+            <div className="ProductInfoText">
+                <h1 className="ProductTitle">{title}</h1>
+                <p className="ProductText">{text}</p>
+                <Link to={to} className="ProductButton">View</Link>
+            </div>
         </div>
     )
 };
