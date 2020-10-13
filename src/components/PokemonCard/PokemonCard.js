d207f57d81f656b8e9174707c1cba4329d01e755
Stoyan Georgiev <stoyangeorgiev1999@gmail.com>
2020-10-13 10:55:00 +0300

    added comments to PokemonCard.js

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -6,13 +6,17 @@ import './PokemonCard.css';
 const imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/`;
 
 const PokemonCard = React.forwardRef(( {pokemon}, ref) => {
-    
+
+
+    //Transforms a number to a 3 digit number: 3 => 003; 23 => 023;
     const TransfromDigit = (number) =>{
         return number = `00${number}`.slice(-3);
     }
 
+    //We get the pokedex number of the pokemon to use it to get the image
     const imgNumber = TransfromDigit(pokemon.id);
 
+    //We loop through the types of the pokemon and create the elements for the card
     const types = pokemon.types.map( ({type, slot}) => (
         <div className={type.name} key={slot}>{type.name}</div>
     ))
