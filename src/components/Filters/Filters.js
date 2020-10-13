94d9d8a44690f698f8447c7a70611cae360caf3a
Stoyan Georgiev <stoyangeorgiev1999@gmail.com>
2020-10-13 10:52:13 +0300

    added comments to Filter.js

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -4,13 +4,16 @@ import './Filters.css';
 
 const Filters = ({active, click, types, filterOptions, changeFilter, apply, reset, toggleOption, toggleType, optionsRef, typesRef}) => {
 
+    //Loops over an array of Pokemon types and creates a button for each of them
     const typesEls = types.map( (el, index) => (
         <button className={`${el.name} filterType`} key={index} onClick={(e) => toggleType(e.target)} >{el.name}</button>
     ));
 
+    //Loops over an array of options depending on the chosen category and creates buttons for them
     const options = filterOptions.map( (el, index) => {
         let name;
 
+        //If generation is chosen we need to change the string
         if(el.name.includes('-')){
             name = el.name.split('-')
             name = `${name[0]} ${name[1].toUpperCase()}`;
@@ -19,6 +22,7 @@ const Filters = ({active, click, types, filterOptions, changeFilter, apply, rese
             name = el.name;
         }
 
+        //Returns a custom checkbox for the options
         return (
             <label className="customInputContainer" key={index} onClick={(e) => toggleOption(e.target)}>
                 {name}
