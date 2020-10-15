aa89bde7bafd4d0c9b9407e9009a5f9f95609c73
Stoyan Georgiev <stoyangeorgiev1999@gmail.com>
2020-10-15 17:25:31 +0300

    added team member styling and logic

--- src/components/TeamMember/TeamMember.js
+++ src/components/TeamMember/TeamMember.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
+import Aux from "../../hoc/Aux";
+
 import './TeamMember.css';
 
 const TeamMember = ({data}) => {
     console.log(data);
+
+    let classes = `TeamMember `;
+    if(data.name == null){
+        classes += data.active ? "active" : "empty";
+    }
+
     return (
-        <div className="TeamMember">
-            {data.name}
+        <div className={classes}>
+            {data.name == null ? null :
+                
+                <Aux>
+                    <img className="MemberImg" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png" />
+                    <div className="MemberInfo">
+                        <span>Pikachu</span>
+                        <span className="PokemonHealthBar"></span>
+                        <span>35/35</span>
+                    </div>
+                </Aux>
+            }
         </div>
     );
 }
--- src/containers/TeamBuilder/TeamBuilder.js
+++ src/containers/TeamBuilder/TeamBuilder.js
@@ -8,7 +8,15 @@ import './TeamBuilder.css';
 import PokeballImg from '../../images/pokeballBG.svg';
 import PokeballIcon from '../../images/pokeballIcon.svg';
 
-const emptyTeam = [{},{},{},{},{},{}]
+const emptyTeam = [
+    {active: true},
+    {active: false},
+    {active: false},
+    {active: false},
+    {active: false},
+    {active: false}
+];
+
 const url = "https://pokeapi.co/api/v2/pokemon/";
 
 const App = () => {
@@ -21,12 +29,9 @@ const App = () => {
                 Team Builder
             </h2>
             <div className="PokemonTeam">
-                <TeamMember data={{name: "Pokemon"}}/>
-                <TeamMember data={{name: "Pokemon"}}/>
-                <TeamMember data={{name: "Pokemon"}}/>
-                <TeamMember data={{name: "Pokemon"}}/>
-                <TeamMember data={{name: "Pokemon"}}/>
-                <TeamMember data={{name: "Pokemon"}}/>
+                {emptyTeam.map((member, index) => {
+                    return <TeamMember data={member} key={index}/>
+                })}
             </div>
             <div className="PokemonInfo">
 
