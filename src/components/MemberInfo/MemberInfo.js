eb480999360b08a123e2f92fcd411759f0ac4ddf
Stoyan Georgiev <stoyangeorgiev1999@gmail.com>
2020-12-26 16:42:35 +0200

    finished team member info mokup

--- src/components/MemberInfo/MemberInfo.js
+++ src/components/MemberInfo/MemberInfo.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Carousel from 'react-elastic-carousel';
+
+import MemberMove from './MemberMove/MemberMove';
 
 import './MemberInfo.css';
 
@@ -9,20 +12,66 @@ const MemberInfo = ({data}) => {
         <div className="MemberInfo">
             <img className="InfoImg" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png" />
             <div className="infoContainer" style={{zIndex: 2}}>
-                <button> &lsaquo; </button>
+                {/* <button> &lsaquo; </button> */}
 
-                <div className="statsContainer">
-                    <h1 className="infoTitle">Stats</h1>
-                    <div className="dataContainer">
-                        <div className="stat">
-                            <div className="statTitle">HP</div>
-                            <div className="statNumber">35</div>
-                            <div className="statLine" style={{width: (35/180) * 100}}></div>
+                <Carousel itemsToShow={1}>
+
+                    <div className="moveContainer">
+                        <h1 className="infoTitle">Moves</h1>
+                        <MemberMove data={{name: 'Thunder', type: "electric", pp: 10}}/>
+                        <MemberMove data={{active: true}}/>
+                        <MemberMove data={{active: false}}/>
+                        <MemberMove data={{active: false}}/>
+                    </div>
+
+                    <div className="statsContainer">
+                        <h1 className="infoTitle">Stats</h1>
+                        <div className="dataContainer">
+
+                            <div className="stat">
+                                <div className="statTitle">HP</div>
+                                <div className="statNumber">35</div>
+                                <div className="statLine" style={{width: `${(35/255) * 100}%`}}></div>
+                            </div>
+
+                            <div className="stat">
+                                <div className="statTitle">Attack</div>
+                                <div className="statNumber">55</div>
+                                <div className="statLine" style={{width: `${(55/255) * 100}%`}}></div>
+                            </div>
+
+                            <div className="stat">
+                                <div className="statTitle">Defence</div>
+                                <div className="statNumber">40</div>
+                                <div className="statLine" style={{width: `${(40/255) * 100}%`}}></div>
+                            </div>
+
                         </div>
                     </div>
-                </div>
 
-                <button> &rsaquo; </button>
+                    <div className="typesContainer">
+                        <h1 className="infoTitle">Stats</h1>
+                        <div className="dataContainer">
+                            <div className="typeInfo">
+                                <div>Weak to:</div>
+                                
+                                <div className="type">
+                                    <div className="type ground">
+                                        <div>Ground</div>
+                                        <div className="typeDmg">2x</div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div>Immune to:</div>
+                            <div>Resistant to:</div>
+                        </div>
+                    </div>
+
+                </Carousel>
+
+               
+                {/* <button> &rsaquo; </button> */}
             </div>
         </div>
     );
---
+++ src/components/MemberInfo/MemberMove/MemberMove.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Aux from "../../../hoc/Aux";
+
+import './MemberMove.css';
+
+const TeamMember = ({data}) => {
+    console.log(data);
+
+    // data.name = null;
+
+    let classes = `MemberMove `;
+    if(data.name == null){
+        classes += data.active ? "active" : "empty";
+    }
+    else{
+        classes += `${data.type} move`;
+    }
+
+    return (
+        <div className={classes}>
+            {data.name == null ? null :
+                <Aux>
+                    <div className="moveName">{data.name}</div>
+                    <div className="movePP">{data.pp} PP</div>
+                </Aux>
+            }
+        </div>
+    );
+}
+
+export default TeamMember;
